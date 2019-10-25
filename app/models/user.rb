9537e393a7b128f952412f406eefa7fbeed3b5f4
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-10-25 12:16:24 -0700

    migration created for digest_password on user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ApplicationRecord
+
+  # constants
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  # relations
+  has_secure_password
+
+  # validations
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 50 },
     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
-    before_save do
-      email.downcase!
-    end
+  # callbacks
+  before_save do
+    email.downcase!
+  end
 end
