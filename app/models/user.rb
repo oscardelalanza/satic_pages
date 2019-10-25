0742c0d1c67c53ef68854b8698b867a3e047ae77
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-10-25 12:33:12 -0700

    testing passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 50 },
     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 
   # callbacks
   before_save do
