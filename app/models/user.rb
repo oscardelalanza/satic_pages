db267f127f699490e24c8c384f3004492badae60
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-10-25 12:09:33 -0700

    downcasing the email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 50 },
     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+    before_save do
+      email.downcase!
+    end
 end
