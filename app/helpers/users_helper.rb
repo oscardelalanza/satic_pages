4cd294a6965913af2c4bc0d63d6a0be412b698a6
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-10-29 10:23:24 -0700

    gravatar image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,8 @@
 module UsersHelper
     # return gravatar for  the given user
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    def gravatar_for(user)
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
 end
