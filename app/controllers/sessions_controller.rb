01eae5be4fef5d7fe48a068d475e3bfed3953006
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-10-31 15:38:15 -0700

    sessionshelper included in application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     
     if user && user.authenticate(params[:session][:password])
-      #  ###
+      # TODO: handle valid authentication
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
