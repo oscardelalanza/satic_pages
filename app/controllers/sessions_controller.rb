a386183ada9c51bcf5de1ba487611b50bbdce4e7
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-01 10:15:24 -0700

    new boolean logged_in method to know if a user is logged

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def new; end
 
+  # create a new session
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     
@@ -13,10 +14,15 @@ class SessionsController < ApplicationController
     end
   end
   
+  def destroy; end
+  
   # this will find the current logged user
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
   
-  def destroy; end
+  # return true if the user is logged in
+  def logged_in?
+    !current_user.nil?
+  end
 end
