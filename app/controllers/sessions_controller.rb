5b76ef3bcf9a7bc4b783f146713d65a1e4e97572
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-01 09:26:14 -0700

    setting up the session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     
     if user && user.authenticate(params[:session][:password])
-      # TODO: handle valid authentication
+      log_in user
+      redirect_to user_url(user)
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
