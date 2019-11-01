5769585105493b2f8afddb1a17b102f3b74e6f4a
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-01 10:29:57 -0700

    wrong placed methods changed from session controller to session helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,14 +15,4 @@ class SessionsController < ApplicationController
   end
   
   def destroy; end
-  
-  # this will find the current logged user
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-  
-  # return true if the user is logged in
-  def logged_in?
-    !current_user.nil?
-  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,14 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # this will find the current logged user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  # return true if the user is logged in
+  def logged_in?
+    !current_user.nil?
+  end
 end
