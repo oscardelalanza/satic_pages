900306f6df0a46833f43d1ea8230ed8095a7ad35
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-04 08:48:15 -0800

    helper method to remove the logged user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  # logs out the current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
   # this will find the current logged user
   def current_user
