5963961ad9314ea8f6a465e8e7a0191ef0007a2b
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-04 13:36:38 -0800

    current_user helper method modified to verify permanent cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,7 +19,16 @@ module SessionsHelper
 
   # this will find the current logged user
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # return true if the user is logged in
