fd86833b09e846aadaa3fb9a88ea4d0766cdf468
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-04 08:16:06 -0800

    user logged automaticly after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
@@ -8,8 +9,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params) # Not the final implementation!
+    @user = User.new(user_params)
+    
     if @user.save
+      log_in(@user) # go to sessions_helper en set a new session
       flash[:success] = "Welcome to the Sample App! " + @user.name
       redirect_to user_url(@user)
     else
