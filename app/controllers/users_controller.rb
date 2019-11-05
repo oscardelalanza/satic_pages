12013e48f69502257b9db1d364edf6d326199399
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-05 12:32:35 -0800

    handling unsuccessful edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update_attributes(user_params)
+      # TODO: handle a successful update
+    else
+      render 'edit'
+    end
+  end
 
   private
 
