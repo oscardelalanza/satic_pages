b83105818e7cb10cea038f73af325cddd9aa6b68
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-07 14:49:41 -0800

    work in progress

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+  
+  # TODO: listing 11.11 The application mailer with a new default from address.
 end
