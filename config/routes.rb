9e8e7465fb9c4ce68af186de38e0eaec0b55d529
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-07 12:28:17 -0800

    resources routes added for account_activations#edit

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
+  # routes for account activation
+  resources :account_activations, only: [:edit]
 end
