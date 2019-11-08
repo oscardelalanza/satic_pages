298e02cc5f66e277e2083453d4bdb7c8cc161cb5
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-08 07:54:09 -0800

    setting the mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'noreply@example.com'
   layout 'mailer'
-  
-  # TODO: listing 11.11 The application mailer with a new default from address.
 end
