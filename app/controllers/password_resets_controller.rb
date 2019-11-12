ee3f840545743f09d0d4f807e153956b3a78fcbb
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-12 10:17:01 -0800

    deprecation fixed and create_reset_digest refactorized

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -27,7 +27,7 @@ class PasswordResetsController < ApplicationController
     if params[:user][:password].empty?                  # Case (3)
       @user.errors.add(:password, "can't be empty")
       render 'edit'
-    elsif @user.update_attributes(user_params)          # Case (4)
+    elsif @user.update(user_params)          # Case (4)
       log_in @user
       flash[:success] = "Password has been reset."
       redirect_to @user
