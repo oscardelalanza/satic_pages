88364507266474aa20e323321b15399f68db0907
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-12 08:05:12 -0800

    new controller en vies generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   # routes for static pages
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
