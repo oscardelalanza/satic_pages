a7f1ed10c7031b700a618d26cc34d4bbe49145c6
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-14 07:39:53 -0800

    micropost content validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ApplicationRecord
   ################################################## Validations #######################################################
   validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
   
   ################################################## Relations #########################################################
   belongs_to :user
