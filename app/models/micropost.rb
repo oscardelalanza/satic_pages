6a851fab65c9527d2a1ab42582cab30e9b4fbe85
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-15 11:53:37 -0800

    validations for image upload

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ApplicationRecord
   ################################################## Validations #######################################################
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
   
   ################################################## Relations #########################################################
   belongs_to :user
@@ -9,4 +10,13 @@ class Micropost < ApplicationRecord
   #################################################### configs #########################################################
   default_scope { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
+  
+  #################################################### private #########################################################
+  private
+  
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
